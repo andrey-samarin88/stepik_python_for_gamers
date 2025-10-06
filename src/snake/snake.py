3a744cb914bc9d54f103226e9d7e5f703be34506
andrey-samarin88 <andrey.samarin88@gmail.com>
2025-10-06 11:54:03 +0300

    add some changes

--- src/snake/snake.py
+++ src/snake/snake.py
@@ -41,9 +41,6 @@ def create_food():
             return (x, y)
 
 
-food = create_food()
-
-
 # Отрисовка еды на игровом поле
 def draw_food():
     canvas.create_rectangle(
@@ -135,26 +132,15 @@ def end_game():
 # Перезапуск игры
 def restart_game():
     global snake, direction, food, score, game_over
-
-    # Начальное положение змейки
-    #snake = [(100, 100), (90, 100), (80, 100)]
-    snake = create_snake()
     direction = "Right"
-
-    # Новая еда
-    food = create_food()
-
-    # Сброс счёта и статуса
     score = 0
     game_over = False
-
-    # Очистим холст и обновим
     canvas.delete("all")
+    food = create_food()
     draw_food()
+    snake = create_snake()
     draw_snake()
     update_title()
-
-    # Перезапускаем игровой цикл
     root.after(DELAY, game_loop)
 
 
@@ -165,16 +151,10 @@ def check_self_collision():
 
 # Создание змейки
 def create_snake():
-    # Вычисляем максимальное значение по X и Y,
-    # чтобы вся змейка (3 клетки) уместилась в пределах поля
     max_x = (WIDTH // CELL_SIZE) - 3
     max_y = (HEIGHT // CELL_SIZE) - 1
-
-    # Случайная позиция головы змейки
     x = random.randint(0, max_x) * CELL_SIZE
     y = random.randint(0, max_y) * CELL_SIZE
-
-    # Возвращаем список из 3 сегментов змейки, направленных вправо
     return [(x, y), (x - CELL_SIZE, y), (x - 2 * CELL_SIZE, y)]
 
 # Игровой цикл
@@ -194,8 +174,9 @@ def game_loop():
 
 
 # Запуск игрового цикла
-snake = create_snake()
+food = create_food()
 draw_food()
+snake = create_snake()
 draw_snake()
 root.after(DELAY, game_loop)
 
